95616179964624065e7276ad8ec9fe4f1ccae82d
eeyll18 <edaeylulgunay2@gmail.com>
2024-08-12 15:14:32 +0300

    add movie section

--- containers/home/index.js
+++ containers/home/index.js
@@ -4,12 +4,15 @@ import React from 'react'
 import Movies from '@/mocks/movies.json'
 import Genres from '@/mocks/genres.json'
 import Categories from '@/components/categories'
+import MovieSection from '@/components/movie-section'
 
 export default function HomeContainer() {
   return (
     <div>
       <FeatureMovie movie={Movies.results[0]}/>
       <Categories categories={Genres.genres.slice(0,5)}/>
+      <MovieSection movies={Movies.results.slice(1,7)} title="Popular Films"/>
+      <MovieSection movies={Movies.results.slice(7,13)} title="Your Favorites"/>
     </div>
   )
 }
