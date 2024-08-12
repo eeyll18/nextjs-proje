13391aa22fb5199db3b2d4ca2af85feae2579d30
eeyll18 <edaeylulgunay2@gmail.com>
2024-08-12 16:25:29 +0300

    add category page loading

---
+++ app/[[...category]]/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import CategoriesLoading from '@/components/categories/loading'
+
+export default function Loading() {
+  return (
+    <div>
+      <CategoriesLoading/>
+    </div>
+  )
+}
